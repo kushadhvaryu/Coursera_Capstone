25a81bbae6726d61caa991ba4675a8f50da94344
Kush Adhvaryu <55607060+kushadhvaryu@users.noreply.github.com>
2021-07-07 10:13:20 -0400

    Add files via upload

---
+++ Week - 1/Coursera Capstone - Week - 1 .py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project Notebook
+# 
+# #### This is the first notebook for the Peer-graded Assignment in the Applied Data Science Capstone Course: Week 1
+
+# In[1]:
+
+
+# importing libraries
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
